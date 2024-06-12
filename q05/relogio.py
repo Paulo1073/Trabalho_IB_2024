1ead44a5c6b94bfe53d08dc844a50fabfd678c8a
Paulo1073 <168665678+Paulo1073@users.noreply.github.com>
2024-06-12 10:08:06 -0300

    relogio.py

--- q05/relogio.py
+++ q05/relogio.py
@@ -4,11 +4,11 @@ hora = int(input("digite um numero entre 0 e 23: "))
 
 if 6 <= hora < 12:
         print(f'São {hora}h da Manhã')
-if 12 <= hora < 18:
+elif 12 <= hora < 18:
          print(f'São {hora}h da Tarde')
-if 17 < hora <= 23:
+elif 17 < hora <= 23:
          print(f'São {hora}h da noite') 
-if 0 <= hora <= 5:
+elif 0 <= hora <= 5:
         print(f'São {hora}h da Madrugada')
 else:
     print('esse Numero Não Esta No Intervalo de (0 a 23)')
